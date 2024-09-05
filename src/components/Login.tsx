0cc78d23912d120594c6a21193d0036dd8ca9193
leonardo <leocoelho.pi@gmail.com>
2024-09-05 20:55:09 -0300

    feat: add login image to login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Login() {
   return (
     <div className="">
-      
+      <img className="w-3/5" src="/assets/images/login-image.png" alt="login-image" />
     </div>
   );
 }
