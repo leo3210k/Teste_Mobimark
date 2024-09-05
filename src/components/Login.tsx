8fbdbba69a36fe457f2f80452c3d0922bdf48f78
leonardo <leocoelho.pi@gmail.com>
2024-09-05 20:47:22 -0300

    refactor: remove unsuned tags

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,20 +2,8 @@ import React from 'react';
 
 function Login() {
   return (
-    <div className="Login">
-      <header className="Login-header">
-        <p>
-          Edit <code>src/Login.js</code> and save to reload.
-        </p>
-        <a
-          className="Login-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      
     </div>
   );
 }
