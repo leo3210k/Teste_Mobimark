de534fcb2a74669f7ebf5155b4d834f839cec7b8
leonardo <leocoelho.pi@gmail.com>
2024-09-06 15:34:19 -0300

    feat: create email field

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -11,6 +11,12 @@ function Login() {
         <div className="relative flex flex-col justify-center items-center font-black text-black mx-auto">
           <h1 className="text-4xl mb-16">Login</h1>
           <form action="" className="flex flex-col gap-8">
+            <div className="flex flex-col gap-y-1">
+              <label htmlFor="" className="">Email</label>
+              <TextField id="outlined-basic" variant="outlined" size="small" className="w-[25rem]" />
+            </div>
+            
+            
           </form>
         </div>
         <img src="/assets/images/login-background.png" className="absolute bottom-0" alt="login-background" />
