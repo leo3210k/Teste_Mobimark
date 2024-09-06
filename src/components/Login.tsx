b282581a41b31e7e8182cb1ed482e616ffa04d61
leonardo <leocoelho.pi@gmail.com>
2024-09-06 15:33:42 -0300

    feat: add form title

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -9,6 +9,7 @@ function Login() {
       <img className="w-7/12 h-screen" src="/assets/images/login-image.png" alt="login-image" />
       <div className="relative w-5/12 flex"> 
         <div className="relative flex flex-col justify-center items-center font-black text-black mx-auto">
+          <h1 className="text-4xl mb-16">Login</h1>
         </div>
         <img src="/assets/images/login-background.png" className="absolute bottom-0" alt="login-background" />
       </div>
