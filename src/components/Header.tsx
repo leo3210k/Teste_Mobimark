60b87a59bffa8c2adf831fc4a336cde3892f4e62
leonardo <leocoelho.pi@gmail.com>
2024-09-06 20:03:53 -0300

    feat: add bg white to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className="">
-      
+    <div className="bg-white">
+      <div></div>
+      <nav></nav>
+      <div></div>
     </div>
   );
 }
