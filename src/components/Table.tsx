46896c99a31552c789e27c6c3f6275d120e25f47
leonardo <leocoelho.pi@gmail.com>
2024-09-08 16:07:02 -0300

    feat: create Table component

---
+++ src/components/Table.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Table() {
+  return (
+    <div className="flex justify-center items-center bg-aqua-haze">
+      <div className="flex flex-col">
+      </div>
+    </div>
+  );
+}
+
+export default Table;
