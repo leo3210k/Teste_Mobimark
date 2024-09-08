2ef63b9462fc26969742dc78e9fe4c7ccf2d6bcd
leonardo <leocoelho.pi@gmail.com>
2024-09-08 16:55:15 -0300

    feat: remove checkbox from table header

--- src/components/SchoolsTable.tsx
+++ src/components/SchoolsTable.tsx
@@ -146,16 +146,7 @@ function EnhancedTableHead(props: EnhancedTableProps) {
   return (
     <TableHead>
       <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              'aria-label': 'select all desserts',
-            }}
-          />
+        <TableCell padding="none">
         </TableCell>
         {headCells.map((headCell) => (
           <TableCell
