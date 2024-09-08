ee5809aa8d0fa331ef5781fd3e267c7d438b6f77
leonardo <leocoelho.pi@gmail.com>
2024-09-08 16:03:13 -0300

    feat: add nav customlinks to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import CustomLink from './utils/CustomLink';
+
 function Header() {
   return (
     <div className="flex items-center bg-white shadow-sm px-6">
       <div className="flex-1"></div>
       <nav className="flex-1 flex justify-center gap-x-12 text-gray-900">
+        <CustomLink href="/" title="Início"/>
+        <CustomLink href="/" title="Escolas"/>
+        <CustomLink href="/" title="Sobre"/>
       </nav>
     </div>
   );
