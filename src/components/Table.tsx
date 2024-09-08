6f0ee345f39acd6f5890691b11f2a9e019b949e9
leonardo <leocoelho.pi@gmail.com>
2024-09-08 16:07:33 -0300

    feat: create Table actions container

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -5,6 +5,8 @@ function Table() {
     <div className="flex justify-center items-center bg-aqua-haze">
       <div className="flex flex-col">
         <h2 className="text-2xl font-bold">Escolas</h2>
+        <div className="flex gap-6">
+        </div>
       </div>
     </div>
   );
