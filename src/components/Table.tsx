4baf9d8d2849d21c1dff0902d77132f7824a07c7
leonardo <leocoelho.pi@gmail.com>
2024-09-08 16:07:17 -0300

    feat: add title to Table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -4,6 +4,7 @@ function Table() {
   return (
     <div className="flex justify-center items-center bg-aqua-haze">
       <div className="flex flex-col">
+        <h2 className="text-2xl font-bold">Escolas</h2>
       </div>
     </div>
   );
