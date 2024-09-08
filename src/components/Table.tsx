494559fd87f61845c6e4688ddf22077f0dfa2e8f
leonardo <leocoelho.pi@gmail.com>
2024-09-08 16:08:39 -0300

    feat: add select to Table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -8,6 +8,21 @@ function Table() {
         <h2 className="text-2xl font-bold">Escolas</h2>
         <div className="flex gap-6">
           <TextField id="outlined-basic" label="Pesquise pelo nome" size="small" variant="outlined" className="w-[30rem]" />
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              size="small"
+              value="age"
+              label="Age"
+              className="w-[10rem]"
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
         </div>
       </div>
     </div>
