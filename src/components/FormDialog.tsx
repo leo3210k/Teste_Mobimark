20bb679eed12290bde38d5e15ab676cbd48e2ea5
leonardo <leocoelho.pi@gmail.com>
2024-09-09 16:54:28 -0300

    chore: remove email variable

--- src/components/FormDialog.tsx
+++ src/components/FormDialog.tsx
@@ -39,7 +39,6 @@ export default function FormDialog() {
             event.preventDefault();
             const formData = new FormData(event.currentTarget);
             const formJson = Object.fromEntries((formData as any).entries());
-            const email = formJson.email;
             console.log(formJson);
             handleClose();
           },
