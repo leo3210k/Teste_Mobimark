3c0f2d3d0d6c97b5c1016a91c2e33443b3a15c5b
leonardo <leocoelho.pi@gmail.com>
2024-09-09 15:16:47 -0300

    feat: add new school button

--- src/components/Schools.tsx
+++ src/components/Schools.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  Button,
   FormControl,
   InputAdornment,
   InputLabel,
@@ -15,38 +16,41 @@ function Schools() {
     <div className="flex justify-center items-center bg-aqua-haze">
       <div className="flex flex-col gap-4">
         <h2 className="text-3xl font-medium mb-6">Escolas</h2>
-        <div className="flex gap-6">
-          <TextField
-            id="outlined-basic"
-            label="Pesquise pelo nome"
-            size="small"
-            variant="outlined"
-            slotProps={{
-              input: {
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <img src="/assets/icons/search.svg" alt="search" />
-                  </InputAdornment>
-                ),
-              },
-            }}
-            className="w-[20rem] !bg-white"
-          />
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Age</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
+        <div className="flex justify-between">
+          <div className="flex gap-6">
+            <TextField
+              id="outlined-basic"
+              label="Pesquise pelo nome"
               size="small"
-              value="age"
-              label="Age"
-              className="w-[10rem] !bg-white"
-            >
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-          </FormControl>
+              variant="outlined"
+              slotProps={{
+                input: {
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <img src="/assets/icons/search.svg" alt="search" />
+                    </InputAdornment>
+                  ),
+                },
+              }}
+              className="w-[20rem] !bg-white"
+            />
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Age</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                size="small"
+                value="age"
+                label="Age"
+                className="w-[10rem] !bg-white"
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <Button variant="contained" className="!text-base !font-mulish !bg-[#6691ff] !normal-case !px-[2.2rem] !py-[0.6rem]">Nova Escola</Button>
         </div>
         <SchoolsTable />
       </div>
