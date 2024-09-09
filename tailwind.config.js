b8f150f1ae10838fafb9efc2ae4b9b4f1c2b5ef8
leonardo <leocoelho.pi@gmail.com>
2024-09-09 15:16:13 -0300

    feat: add mulish font to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        'mulish': ['Mulish', 'sans-serif']
+      },
       colors: {
         "royal-blue": "#5047ed",
         "aqua-haze": "#F6F8FA"
