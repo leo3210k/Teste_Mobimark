35e163c0af23d70ef845e6d78be7506ef8f3e913
leonardo <leocoelho.pi@gmail.com>
2024-09-09 20:56:23 -0300

    chore: create api file

---
+++ src/components/utils/Api.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const BASE_URL = 'https://apiteste.mobieduca.me/api';
+
+const TOKEN = '45|YHAUKjrAHBw3LAQyO1rDj1vHQACYGWkDBfpmvg5cbed03fd4';
+export const CONFIG = {
+  headers: { Authorization: `Bearer ${TOKEN}` }
+}
