4cdb6ed05aea45b4983d96e3aeb1c16156168377
leonardo <leocoelho.pi@gmail.com>
2024-09-11 19:24:01 -0300

    chore: remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
-import App from './main/App';
-import Login from './components/Login'
-import Main from './components/Main'
 import router from './config/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
