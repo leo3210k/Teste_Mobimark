84d427204e37b3e850a69141f2e765403d4e7d87
leonardo <leocoelho.pi@gmail.com>
2024-09-12 00:47:09 -0300

    feat: add Header to App component

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import Header from '../components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="h-screen grid grid-rows-[80px,1fr]">
+      <Header />
       <Outlet />
     </div>
   );
