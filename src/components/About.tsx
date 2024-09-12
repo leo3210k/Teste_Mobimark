cb62848feeff5d753211568ce01ab182839fd5ec
leonardo <leocoelho.pi@gmail.com>
2024-09-12 00:45:38 -0300

    feat: create about component

---
+++ src/components/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="flex items-center bg-white shadow-sm px-6">
+      Teste
+    </div>
+  );
+}
+
+export default About;
