7a17ac3df2945ed52a1dc101bcf27ac4cd3ece77
leonardo <leocoelho.pi@gmail.com>
2024-09-12 04:36:59 -0300

    feat: add xs on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'xs': '500px'
+        'xs': '500px',
       },
       fontFamily: {
         'mulish': ['Mulish', 'sans-serif']
